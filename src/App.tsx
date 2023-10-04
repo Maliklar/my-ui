0ad4f6222e535f64899298a89803d2020caa8982
Malik Elbadri <elbadrimalik@gmail.com>
2023-10-04 18:34:58 +0300

    Updated button properties

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,43 @@ import "./App.css";
 import Avatar from "./components/Avatar";
 import Button from "./components/Button";
 import TextField from "./components/TextField";
-import { FaAccusoft, FaExclamationTriangle } from "react-icons/fa";
-import Color from "./types/styles/colors";
+import {
+  FaAccusoft,
+  FaAdjust,
+  FaExclamationCircle,
+  FaExclamationTriangle,
+  FaPlay,
+} from "react-icons/fa";
 import Dialog from "./components/Dialog";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Alert from "./components/Alert";
+import Color from "./types/styles/colors";
+
+type Colors =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "error"
+  | "warning"
+  | "none";
+
+const colors = ["primary", "secondary", "success", "error", "warning", "none"];
 function App() {
   const [open, setOpen] = useState(false);
+  const [color, setColor] = useState<Color>(colors[0] as Color);
+
+  useEffect(() => {
+    let i = 0;
+    let interval = setInterval(() => {
+      if (i >= colors.length) i = 0;
+      if (colors[i]) setColor(colors[i] as Color);
+      i++;
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div
       className="App"
@@ -17,9 +47,33 @@ function App() {
         height: 4000,
       }}
     >
-      <Button variant="outlined" color="primary">
-        Hello
-      </Button>
+      <section>
+        <h2>Buttons</h2>
+        <div
+          style={{
+            display: "flex",
+            gap: "1rem",
+            flexWrap: "wrap",
+          }}
+        >
+          <Button
+            variant="contained"
+            color={color}
+            startIcon={FaExclamationCircle}
+          >
+            Contained
+          </Button>
+
+          <Button variant="outlined" color={color} startIcon={FaAdjust}>
+            Outlined
+          </Button>
+
+          <Button variant="text" color={color} startIcon={FaPlay}>
+            Text
+          </Button>
+        </div>
+      </section>
+
       <TextField
         title="SOME INPUT"
         placeholder="Hello World"
